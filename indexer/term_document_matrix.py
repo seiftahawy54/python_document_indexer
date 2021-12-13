8d12b98c34e2241aba1761bdba5daac1d580684b
Seif Tahawy <seiftahawy54@gmail.com>
2021-12-14 01:10:06 +0200

    part 1 in my task

--- indexer/term_document_matrix.py
+++ indexer/term_document_matrix.py
@@ -2,6 +2,8 @@ from . tokenizer import Tokenizer
 from dataclasses import dataclass
 from scipy.sparse import spmatrix
 from . document_collection import DocumentCollection
+import os
+
 
 @dataclass
 class TermDocumentMatrix:
@@ -19,7 +21,26 @@ class TermDocumentMatrix:
         self.computeMatrix()
 
     def computeMatrix(self):
-        raise NotImplementedError
+        alluniquewords = dict()
+        # firstdoc = open("../sample_collection/001", "r")
+        # paragraphs = firstdoc.read()
+
+        allwantedfiles = os.listdir('../sample_collection')
+
+        for file in allwantedfiles:
+            currentfile = open('../sample_collection/' + file, "r")
+            filecontent = currentfile.read()
+
+            for word in filecontent.split():
+                word = word.lower()
+                if word in alluniquewords:
+                    alluniquewords[str(word)] += 1
+                else:
+                    alluniquewords[str(word)] = 1
+
+        writing_file = open("unique_words", "w+")
+        for words in alluniquewords:
+            writing_file.write(words + " : " + str(alluniquewords[words]))
 
     #######
     # Query
